f2158c3bbe5364f5b67d8ca0eb962b0ffc683f36
Frank Obeng <obengfan@gmail.com>
2023-08-13 16:31:55 +0200

    made changes with the react app

--- Front-end/ghamro-frontend/src/Components/Navbar.js
+++ Front-end/ghamro-frontend/src/Components/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import bootstrap from 'bootstrap'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar = () => {
+
+  Navbar = () => {
   return (
-    <div>
+    <>
+       <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      </>
 
-    </div>
+
   )
 }
 
-export default Navbar
+export default Navbar;
